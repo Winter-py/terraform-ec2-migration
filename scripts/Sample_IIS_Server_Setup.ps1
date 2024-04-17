d1d723958ecf9ff27d218e3afc9700a507947310
Steven Owatemi <isosteven@gmail.com>
2024-04-17 10:22:29 +0100

    Don't see changes

--- scripts/Sample_IIS_Server_Setup.ps1
+++ scripts/Sample_IIS_Server_Setup.ps1
@@ -51,6 +51,7 @@ $sqlauth | Set-Content $ENV:TEMP\auth.sql
 $sqlscript | Set-Content $ENV:TEMP\master.sql
 $EXITCODE = sqlcmd.exe -S localhost -E -i $ENV:TEMP\master.sql
 $AUTHCODE = sqlcmd.exe -S localhost -E -i $ENV:TEMP\auth.sql
+
 if($EXITCODE -ne 0){
     Write-Host "Database command status good: $EXITCODE"
 }else {
